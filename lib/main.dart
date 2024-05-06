5412172944acaa50c54cbdf7c32775183e682486
Muhammad Osama Noor <100303780+Meizzosama@users.noreply.github.com>
2024-05-06 11:04:39 +0500

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:todo_app/splash_screen/splash_screen.dart';
 
-void main() => runApp(TodoApp());
+void main() => runApp(const TodoApp());
 
 class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
--- lib/splash_screen/splash_screen.dart
+++ lib/splash_screen/splash_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todo_app/todo_list/todo.dart';
@@ -9,98 +11,102 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        fit: StackFit.passthrough,
-        children: [
-          Column(
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Stack(
+            fit: StackFit.passthrough,
             children: [
-              SizedBox(
-                height: 480,
-                width: 400,
-                child: Image.asset(
-                  'assets/profile.png',
-                  fit: BoxFit.cover,
-                ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              const Row(
+              Column(
                 children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 35.0),
-                    child: Text(
-                      "Are you ready",
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w400,
-                        fontFamily: "Montserrat",
+                  SizedBox(
+                    height: 480,
+                    width: 400,
+                    child: Image.asset(
+                      'assets/profile.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  const Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 35.0),
+                        child: Text(
+                          "Are you ready",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: "Montserrat",
+                          ),
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
-              const Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 35.0, top: 20),
-                    child: Text(
-                      "to build some habits?",
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w500,
-                        fontFamily: "Montserrat",
+                  const Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 35.0, top: 20),
+                        child: Text(
+                          "to build some habits?",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: "Montserrat",
+                          ),
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
-              Container(
-                width: double.infinity,
-                margin: const EdgeInsets.all(30),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Get.to(() => TodoList(),
-                        transition: Transition.rightToLeft);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFFEBE983),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                  ),
-                  child: Container(
+                  Container(
                     width: double.infinity,
-                    alignment: Alignment.center,
-                    child: const Text(
-                      'Continue',
-                      style: TextStyle(fontSize: 18),
+                    margin: const EdgeInsets.all(30),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Get.to(() => TodoList(),
+                            transition: Transition.rightToLeft);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFFEBE983),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                      ),
+                      child: Container(
+                        width: double.infinity,
+                        alignment: Alignment.center,
+                        child: const Text(
+                          'Continue',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  IconButton(
-                    icon: const Icon(
-                      Icons.arrow_forward,
-                      size: 30,
-                    ),
-                    onPressed: () {
-                      Get.to(() => TodoList(),
-                          transition: Transition.rightToLeft);
-                    },
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(
+                        icon: const Icon(
+                          Icons.arrow_forward,
+                          size: 30,
+                        ),
+                        onPressed: () {
+                          Get.to(() => TodoList(),
+                              transition: Transition.rightToLeft);
+                        },
+                      ),
+                    ],
                   ),
                 ],
               ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
